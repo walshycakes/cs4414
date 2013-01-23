a73f963148ea899bdde0c80ef679093af660571e
jaredculp <jjc4fb@virginia.edu>
2013-01-23 11:06:30 -0500

    working visitor func

--- lab1/list.c
+++ lab1/list.c
@@ -5,22 +5,28 @@
 void list_init(list_t *l, 
 		int (*compare)(const void *key, const void *with),
 		void (*datum_delete)(void *datum)) {
+	l->head = NULL;
+	l->tail = NULL;
+	l->length = 0;
+
+	//TODO function pointers
 }
 
 void list_visit_items(list_t *l, void (*visitor)(void *v)) {
-	
 	list_item_t *node = l->head;
-	while(node->next != NULL){
-		visitor(node);
+	while (node->next != NULL){
+		char* datum = node->datum;
+		visitor((void*) datum);
 		node = node->next;
 	}
+	visitor((void*) node->datum);
 }
-
 
 void list_insert_tail(list_t *l, void *v) {
 	// create node
 	list_item_t* tail = (list_item_t*) malloc(sizeof(list_item_t));
 	tail->datum = v;
+	l->length++;
 
 	// grab existing tail, set up pointers
 	list_item_t* node = l->tail;
@@ -40,30 +46,8 @@ void list_insert_tail(list_t *l, void *v) {
 }
 
 void list_insert_sorted(list_t *l, void *v) {
-	
-	
+	// TODO use qsort and comparison function
 }
-
 
 void list_remove_head(list_t *l) {
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- lab1/listharness.c
+++ lab1/listharness.c
@@ -20,83 +20,52 @@ int main (int argc, char* argv[]) {
 	visitor = &visitor_string;
 	list_t* theList = (list_t*) malloc(sizeof(list_t));
 
+	int notEcho = 0;
+
 	FILE* fp = fopen(filename, "r");
 	if (fp != NULL) {
 		char buffer[MAX_CHARS];
 		while (fgets(buffer, MAX_CHARS, fp) != NULL) {
 			if (strcmp(command, "echo") == 0) {
-				//printf("%s", buffer);
-				
-				list_visit_items(theList, visitor);
-				
+				printf("%s", buffer);	
 			}
 			else {
+				notEcho = 1;
 				char* datum = (char*) malloc((strlen(buffer)+1) * sizeof(char));
 				strcpy(datum, buffer);
 
-				if (strcmp(command, "sort") == 0) {
-				}
-				else if (strcmp(command, "tail") == 0) {
-					list_insert_tail(theList, (void*) datum);
-				}
-				else if (strcmp(command, "tail-remove") == 0) {
-				}
-				else {
-					perror("Invalid command. Must be echo, sort, tail, tail-remove");
-					exit(-1);
-				}
+				list_insert_tail(theList, (void*) datum);
 			}
+		}
+		if (notEcho) {
+			list_visit_items(theList, visitor);
 		}
 	}
 
-	list_item_t* head = theList->head;
-	int i = 0;
-	while(head->next != NULL) {
-		printf("node # : %d data : %s\n", i, (char*) head->datum);
-		++i;
-		head = head->next;
-	}
+	// if (strcmp(command, "sort") == 0) {
+	// }
+	// else if (strcmp(command, "tail") == 0) {
+	// 	list_insert_tail(theList, (void*) datum);
+	// 	list_visit_items(theList, visitor);
+	// }
+	// else if (strcmp(command, "tail-remove") == 0) {
+	// }
+	// else {
+	// 	perror("Invalid command. Must be echo, sort, tail, tail-remove");
+	// 	exit(-1);
+	// }
 	
 	return 0;
 }
 
 //asumes we're passing datum to be compared between 2 nodes
-int compare(const void *key, const void *with){
-	
+int compare_string(const void *key, const void *with){
 	char* left = (char*) key;
 	char* right = (char*) with;
 	
 	return strcmp(left,right);
-
 }
 
-void visitor_string(void* v){
-
-	list_item_t* x = (list_item_t*) v;
-	
-	printf("%s\n",(char*) x->datum);
-		
-
+void visitor_string(void* v) {
+	printf("%s",(char*) v);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
