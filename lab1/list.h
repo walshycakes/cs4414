f5142f05fd1b84890181e4cb328ce8c483d245e7
walshycakes <bpw7xx@virginia.edu>
2013-01-25 17:41:05 -0500

    final submission

--- lab1/list.h
+++ lab1/list.h
@@ -13,12 +13,15 @@ typedef struct list {
 	void (*datum_delete)(void *);
 } list_t;
 
+/* pass a list, compare function with two list_item_t, delete function with list_item_t */
 void list_init(list_t *l, 
 	int (*compare)(const void *key, const void *with),
 	void (*datum_delete)(void *datum));
 
+/* pass a list, visitor function with list_item_t */
 void list_visit_items(list_t *l, void (*visitor)(void *v));
 
+/* pass a list, list_item_t */
 void list_insert_tail(list_t *l, void *v);
 
 void list_insert_sorted(list_t *l, void *v);
