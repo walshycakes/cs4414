fa2b4ce865cc841fca47e28482680fce5a39e21a
jaredculp <jjc4fb@virginia.edu>
2013-02-01 18:18:09 -0500

    working exit

--- lab2/main.c
+++ lab2/main.c
@@ -13,38 +13,37 @@ int main(int argc, char** argv) {
 	int is_true = 1;
 
 	while (is_true) {
-		pid = fork();
-		if (pid < 0) {
-			fprintf(stderr, "Fork failed");
-			return 1;
+		printf("$");
+		// parse input
+		char *path = "/bin/";
+
+		char buf[MAX_LINE];
+		fgets(buf, MAX_LINE+1, stdin);
+		int len = strlen(buf);
+		if (buf[len-1] == '\n') {
+			buf[len-1] = '\0';
 		}
-		else if (pid == 0) { // child	
-			printf("$");
 
-			// parse input
-			char *path = "/bin/";
+		strcpy(command, path);
+		strcat(command, buf);
+		if (strcmp(buf, "exit") == 0) {
+			is_true = 0;
+		}
+		else {
+			pid = fork();
 
-			char buf[MAX_LINE];
-			fgets(buf, MAX_LINE+1, stdin);
-			int len = strlen(buf);
-			if (buf[len-1] == '\n') {
-				buf[len-1] = '\0';
+			if (pid < 0) {
+				fprintf(stderr, "Fork failed");
+				return 1;
 			}
-
-			strcpy(command, path);
-			strcat(command, buf);
-
-			printf("child: pid = %d\n", pid);
-			if (strcmp(buf, "exit") == 0) {
-				is_true = 0;
-			}
-			else {
+			else if (pid == 0) { // child
+				printf("child: pid = %d\n", pid);
 				execlp(command, buf, NULL);
 			}
-		}
-		else { // parent
-			printf("parent: pid = %d\n", pid);
-			wait(NULL);
+			else { // parent
+				printf("parent: pid = %d\n", pid);
+				wait(NULL);
+			}
 		}
 	}
 
