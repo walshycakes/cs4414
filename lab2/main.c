d674cf0b51cbcc237fc8ccfdf4401d7c9bd99b59
jaredculp <jjc4fb@virginia.edu>
2013-02-01 18:10:25 -0500

    working exec

--- lab2/main.c
+++ lab2/main.c
@@ -1,15 +1,51 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX_LINE 80
 
 int main(int argc, char** argv) {
-	char *args[MAX_LINE/2 + 1];
-	int should_run = 1;
+	pid_t pid;
+	char command[MAX_LINE];
 
-	while (should_run) {
-		printf("shell>");
-		fflush(stdout);
+	int is_true = 1;
+
+	while (is_true) {
+		pid = fork();
+		if (pid < 0) {
+			fprintf(stderr, "Fork failed");
+			return 1;
+		}
+		else if (pid == 0) { // child	
+			printf("$");
+
+			// parse input
+			char *path = "/bin/";
+
+			char buf[MAX_LINE];
+			fgets(buf, MAX_LINE+1, stdin);
+			int len = strlen(buf);
+			if (buf[len-1] == '\n') {
+				buf[len-1] = '\0';
+			}
+
+			strcpy(command, path);
+			strcat(command, buf);
+
+			printf("child: pid = %d\n", pid);
+			if (strcmp(buf, "exit") == 0) {
+				is_true = 0;
+			}
+			else {
+				execlp(command, buf, NULL);
+			}
+		}
+		else { // parent
+			printf("parent: pid = %d\n", pid);
+			wait(NULL);
+		}
 	}
 
 	return 0;
