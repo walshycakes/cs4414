639e2b40db5e70c3b90795d3cfa249b59ae12cef
jaredculp <jjc4fb@virginia.edu>
2013-02-07 00:25:23 -0500

    semi-working wildcard

--- lab2/main.c
+++ lab2/main.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <glob.h>
+#include <assert.h>
 
 /* we assume max chars entered is 80 */
 #define MAX_LINE 80
@@ -14,33 +16,38 @@ unsigned int is_redirect_out = 0;
 unsigned int is_redirect_in = 0;
 /* global placeholder for redirection path */
 char *redirect_stream = NULL;
+/* global flag for wildcard handling */
+unsigned int has_wildcard = 0;
+/* global placebolder for wildcard expression */
+char *wildcard_expr = NULL;
+
+glob_t globbuf;
 
 char** tokenizeInput(char *buf) {
-	int n = 0;
 	char **argv = (char**) calloc(1, sizeof(char*));
-	
 	fgets(buf, MAX_LINE+1, stdin);
 
-	// if the buffer contains an '&', lower is_fg flag
-	if (strchr(buf, '&') != NULL) {
-		is_fg = 0;
-	}
-
-	// determine is stdout or stdin need to be redirected
+	// set all global flags
+	is_fg = (strchr(buf, '&') != NULL) ? 0 : 1;
 	is_redirect_out = (strchr(buf, '>') != NULL) ? 1 : 0;
 	is_redirect_in = (strchr(buf, '<') != NULL) ? 1 : 0;
+	has_wildcard = (strchr(buf, '*') != NULL) ? 1 : 0;
 
 	char *tokens = strtok(buf, " \n");
-
+	int n = 0;
 	while (tokens) {
 		// dynamically allocate space for this param
 		argv[n] = (char*) calloc(strlen(tokens), sizeof(char));
 		
-		// once we hit a redirect pipe, stop building argv
+		// if redirect pipe, store redirect stream and advance tokens
 		if (strcmp(tokens, ">") == 0 || strcmp(tokens, "<") == 0) {
-			argv[n] = NULL;
 			redirect_stream = strtok(NULL, " \n");
-			break;
+			tokens = strtok(NULL, " \n");
+		}
+		else if (strchr(tokens, '*') != NULL) {
+			wildcard_expr = tokens;
+			tokens = strtok(NULL, " \n");
+			tokens = strtok(NULL, " \n");
 		}
 		argv[n] = tokens;
 		++n;
@@ -53,63 +60,63 @@ char** tokenizeInput(char *buf) {
 	return argv;
 }
 
-void redirectStream(char** argv) {
-	// TODO
-}
-
-void restoreStream() {
-	// TODO
-}
-
 int main(int argc, char** argv) {
+	// fd[0] input pipe, fd[1] output pipe
+	int fd[2];
+	pipe(fd);
+
 	pid_t pid;
-	char command[MAX_LINE];
 
-	int is_true = 1;
-
-	while (is_true) {
+	while (1) {
 		printf("$");
 
 		char buf[MAX_LINE];
 		char **argvv = tokenizeInput(buf);
 
 		if (strcmp(argvv[0], "exit") == 0) {
-			is_true = 0;
-			kill(pid, SIGTERM);
-			return 0;
+			exit(1);
 		}
-		else {
-			pid = fork();
 
-			if (pid < 0) {
-				fprintf(stderr, "Fork failed");
-				return 1;
-			}
-			else if (pid == 0) { // child
-				// backup stdout, stdin
-				int stdin_save, stdout_save;
+		pid = fork();
 
-				// exec, redirecting streams as necessary
-				if (is_redirect_in) {
-					stdin_save = dup(STDIN_FILENO);
-					freopen(redirect_stream, "r", stdin);
-					execvp(argvv[0], argvv);
-					dup2(stdin_save, STDIN_FILENO);
-				}
-				else if (is_redirect_out) {
-					stdout_save = dup(STDOUT_FILENO);
-					freopen(redirect_stream, "w", stdout);
-					execvp(argvv[0], argvv);
-					dup2(stdout_save, STDOUT_FILENO);
-				}
-				else {
-					execvp(argvv[0], argvv);
-				}
+		if (pid < 0) {
+			fprintf(stderr, "Fork failed");
+			exit(1);
+		}
+		else if (pid == 0) { // child
+			// backup stdout, stdin
+			int stdin_save, stdout_save;
+
+			// exec, redirecting streams as necessary
+			if (is_redirect_in) {
+				stdin_save = dup(STDIN_FILENO);
+				freopen(redirect_stream, "r", stdin);
+				execvp(argvv[0], argvv);
+				dup2(stdin_save, STDIN_FILENO);
+				exit(1);
 			}
-			else { // parent
-				if (is_fg) {
-					wait(NULL);
-				}
+			else if (is_redirect_out) {
+				stdout_save = dup(STDOUT_FILENO);
+				freopen(redirect_stream, "w", stdout);
+				execvp(argvv[0], argvv);
+				dup2(stdout_save, STDOUT_FILENO);
+				exit(1);
+			}
+			else if (has_wildcard) {
+				// usage as detailed in `man glob`
+				glob(wildcard_expr, 0, NULL, &globbuf);
+				int match_count = globbuf.gl_pathc;
+				execvp(argvv[0], globbuf.gl_pathv);
+				exit(1);
+			}
+			else {
+				execvp(argvv[0], argvv);
+				exit(1);
+			}
+		}
+		else { // parent
+			if (is_fg) {
+				wait(NULL);
 			}
 		}
 	}
