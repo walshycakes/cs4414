4543570abe8bed0be82d41bc36fdcde0d11fbeb7
jaredculp <jjc4fb@virginia.edu>
2013-02-19 11:54:09 -0500

    stubbed out lab3

---
+++ lab3/threadpkg1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	return 0;
+}
+
+void uthread_init() {
+}
+
+int uthread_create(void (*func)(int), int val, int pri) {
+	return 0;
+}
+
+void uthread_yield() {
+}
+
+void uthread_exit() {
+}
